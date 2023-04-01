6fa34dde624e1c0da64df338c2547388a0adb29d
urluconeptual <ciocan_alexandra16@yahoo.com>
2023-04-01 17:13:35 +0300

    remove lombok annotations from edition

--- src/main/java/ro/project/model/Edition.java
+++ src/main/java/ro/project/model/Edition.java
@@ -1,15 +1,7 @@
 package ro.project.model;
 
-import lombok.*;
-import lombok.experimental.SuperBuilder;
 import ro.project.model.enums.EditionFormat;
 
 import java.time.LocalDate;
 
-@SuperBuilder
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-@Setter
 public record Edition(EditionFormat format, LocalDate publishingDate, String publisher) { }
